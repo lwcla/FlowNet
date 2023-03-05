0ab5ef79678e261322978e0b15cc54845b049135
Walter Pena <18883278692@163.com>
2023-03-05 18:00:41 +0800

    删除测试方法

--- app/src/main/java/cn/cla/net/demo/vm/MainVm.kt
+++ app/src/main/java/cn/cla/net/demo/vm/MainVm.kt
@@ -1,9 +1,6 @@
 package cn.cla.net.demo.vm
 
 import android.app.Application
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.liveData
-import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import cn.cla.library.net.vm.BaseViewModel
 import kotlinx.coroutines.launch
@@ -17,32 +14,10 @@ class MainVm(app: Application) : BaseViewModel(app) {
         repo.loadHomeBanner()
     }
 
-    private val _liveData = MutableLiveData<Int>()
-    val liveData = _liveData.switchMap { params ->
-        val list = mutableListOf<String>()
-        repeat(10) {
-            list.add("${params}-$it")
-        }
-
-        liveData {
-            emit(list)
-        }
-    }
-
-
-    /**
-     * 首页banner
-     */
+    /** 首页banner */
     fun loadHomeBanner(force: Boolean) {
         viewModelScope.launch {
             _homeDataFlow.setValue("这是设置的值", force)
         }
     }
-
-    private var index = 0
-
-    fun liveDataTest() {
-        _liveData.postValue(index++)
-    }
-
 }
